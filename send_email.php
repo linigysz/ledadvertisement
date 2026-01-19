c2f984e6d3c9154106bab1325f33c1d34cdd8ce6
linigysz <lini@gysztechnologies.com>
2026-01-19 17:29:06 +0530

    SMTP password Setup

--- send_email.php
+++ send_email.php
@@ -56,14 +56,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();                                            
         $mail->Host       = 'smtp.example.com';                     // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'your_email@example.com';               // SMTP username
-        $mail->Password   = 'your_password';                        // SMTP password
+        $mail->Username   = 'gysztechnologies@gmail.com';               // SMTP username
+        $mail->Password   = 'pfgnmktbjqqglpak';                        // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('info@gysztechnologies.com');     // Add a recipient
+        $mail->setFrom('gysztechnologies@gmail.com', 'Mailer');
+        $mail->addAddress('lini@gysztechnologies.com');     // Add a recipient
         $mail->addReplyTo($email, $name);
 
         //Content
